cff85afabee66f590e5f15643ce1b7ae475513e3
Simas <simonas@digitouch.lt>
2019-05-16 08:20:49 +0300

    vue boilerplate

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <product-list-one></product-list-one>
+    <product-list-two></product-list-two>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {
+      products: [
+        { name: 'Banana Skin', price: 20 },
+        { name: 'Shiny Star', price: 40 },
+        { name: 'Green Shells', price: 60 },
+        { name: 'Red Shells', price: 80 },
+      ],
+    };
+  },
+};
+</script>
